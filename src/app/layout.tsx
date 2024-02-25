d216d3111a9aa7a3cd4255a02a4d299336778248
Lucas Tenorio <tnrlucas@gmail.com>
2024-02-25 17:22:39 -0300

    Update body classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} bg-background text-txt-primary`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
