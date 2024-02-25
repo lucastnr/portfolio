089a0f995cf231c945afe390d7bffbdf31df64f9
Lucas Tenorio <tnrlucas@gmail.com>
2024-02-25 18:46:30 -0300

    Update skills gap

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -3,7 +3,7 @@ const skills = ["Language", "Language2", "Language3", "Languag4", "Language5"];
 
 export function SkillsSection() {
   return (
-    <div>
+    <div className="flex flex-wrap gap-2">
       {skills.map((skill) => (
         <p
           className="bg-primary inline-block rounded-xl px-4 py-2 font-medium"
