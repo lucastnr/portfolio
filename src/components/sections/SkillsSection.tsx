bccb4235588f2af06e68f3c16715097a18d35b44
Lucas Tenorio <tnrlucas@gmail.com>
2024-02-25 18:45:11 -0300

    Remove margin in SkillsSection component

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -6,7 +6,7 @@ export function SkillsSection() {
     <div>
       {skills.map((skill) => (
         <p
-          className="bg-primary inline-block rounded-xl px-4 py-2 mr-2 font-medium"
+          className="bg-primary inline-block rounded-xl px-4 py-2 font-medium"
           key={skill}
         >
           {skill}
