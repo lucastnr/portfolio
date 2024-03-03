be2f9f3d133316072967316d935b104d4d17dff6
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 20:51:21 -0300

    Update dependencies and add Background component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import { Background } from "@/components/Background";
 import { TypedAnimation } from "@/components/TypedAnimation";
 
 export default function Home() {
   return (
     <main>
-      <div className="min-h-screen">
-        <div className="p-32 max-w-[90%] text-txt-secondary">
-          <TypedAnimation className="font-medium text-8xl">
+      <div className="min-h-screen relative overflow-x-hidden">
+        <div className="absolute z-0 w-screen min-w-[1300px] max-w-[1700px] h-[100%] right-0 xl:left-0">
+          <Background />
+        </div>
+        <div className="p-32 w-[80%] max-w-[1200px] text-txt-secondary z-1">
+          <TypedAnimation className="font-medium text-4xl">
             {"<Lucas Tenorio/>"}
           </TypedAnimation>
-          <TypedAnimation className="font-light mt-6 text-lg" ms={40}>
+          <TypedAnimation className="font-light mt-2 text-lg" ms={40}>
             A talented Software Developer with 4+ years of experience building
             production-ready products to companies.
           </TypedAnimation>
---
+++ src/components/Background.tsx
@@ -0,0 +1,48 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import {
+  EffectComposer,
+  HueSaturation,
+  Noise,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import { Suspense, useRef } from "react";
+
+function Box() {
+  const meshRef = useRef<any>(null!);
+
+  useFrame((state, delta) => {
+    meshRef.current.rotation.z += 0.01;
+    meshRef.current.rotation.x += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef} position={[-1.6, -1.4, -2]}>
+      <sphereGeometry args={[6, 100, 100]} />
+      <meshStandardMaterial color="#362938" />
+    </mesh>
+  );
+}
+
+export function Background() {
+  return (
+    <Canvas
+      gl={{
+        antialias: true,
+      }}
+      style={{
+        width: "100%",
+        maxWidth: "2000px",
+        height: "100%",
+      }}
+    >
+      <Suspense fallback={null}>
+        <Box />
+        <pointLight intensity={1.3} position={[3, 1, 4]} />
+        <EffectComposer>
+          <Noise opacity={0.3} blendFunction={BlendFunction.ADD} />
+          <HueSaturation hue={0.1} saturation={0.3} />
+        </EffectComposer>
+      </Suspense>
+    </Canvas>
+  );
+}
