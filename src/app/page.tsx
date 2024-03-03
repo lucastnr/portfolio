c3e7724b0a801b4962d707e7cfbe3141b41cb7ac
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 01:24:47 -0300

    Refactor Home component to include a profile section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div className="min-h-screen">
+        <div className="p-32 max-w-[60%] text-txt-secondary">
+          <h2 className="font-semibold text-6xl">{"<Lucas Tenorio/>"}</h2>
+          <p className="font-light mt-6">
+            A talented Software Developer with 4+ years of experience building
+            production-ready products to companies.
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
