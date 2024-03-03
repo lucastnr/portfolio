9cc9a8a11f2af06380c148692db6c010a057c813
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 18:25:09 -0300

    Update styles for title on page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,11 @@ export default function Home() {
   return (
     <main>
       <div className="min-h-screen">
-        <div className="p-32 max-w-[60%] text-txt-secondary">
-          <h2 className="font-semibold text-6xl">{"<Lucas Tenorio/>"}</h2>
-          <TypedAnimation className="font-light mt-6">
+        <div className="p-32 max-w-[90%] text-txt-secondary">
+          <TypedAnimation className="font-medium text-8xl">
+            {"<Lucas Tenorio/>"}
+          </TypedAnimation>
+          <TypedAnimation className="font-light mt-6 text-lg" ms={40}>
             A talented Software Developer with 4+ years of experience building
             production-ready products to companies.
           </TypedAnimation>
