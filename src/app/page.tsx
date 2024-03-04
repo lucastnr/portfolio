b1fe25a4415b4bf8f8234eae3b68665af13ef5dc
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 21:24:58 -0300

    Update z-indexes from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export default function Home() {
   return (
     <main>
       <div className="min-h-screen relative overflow-x-hidden">
-        <div className="absolute z-0 w-screen min-w-[1300px] max-w-[2200px] h-[100%] right-0 xl:left-0">
+        <div className="absolute z-[-1] w-screen min-w-[1300px] max-w-[2200px] h-[100%] right-0 xl:left-0">
           <Background />
         </div>
-        <div className="p-32 w-[80%] max-w-[1200px] text-txt-secondary z-1">
+        <div className="p-32 w-[80%] max-w-[1200px] text-txt-secondary">
           <TypedAnimation className="font-medium text-4xl">
             {"<Lucas Tenorio/>"}
           </TypedAnimation>
