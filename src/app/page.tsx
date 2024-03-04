e2306db776c9325b50f8cabe71680dbcd5119bfd
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 23:26:20 -0300

    Add Links component to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Background } from "@/components/Background";
+import { Links } from "@/components/Links";
 import { Subtitle } from "@/components/Subtitle";
 import { Title } from "@/components/Title";
 
@@ -6,12 +7,13 @@ export default function Home() {
   return (
     <main>
       <div className="min-h-dvh relative overflow-x-hidden flex justify-center items-center">
-        <div className="absolute w-dvw min-w-[700px] max-w-[2200px] h-[100%] right-0 xl:left-0">
+        <div className="absolute z-[-1] w-dvw min-w-[700px] max-w-[2200px] h-[100%] right-0 xl:left-0">
           <Background />
         </div>
         <div className="w-[80%] xl:w-[1210px]mb-24">
           <Title />
           <Subtitle />
+          <Links />
         </div>
       </div>
     </main>
