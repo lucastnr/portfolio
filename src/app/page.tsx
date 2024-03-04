ca9edf62f5c032024113035550879c6ac2481abe
Lucas Tenorio <tnrlucas@gmail.com>
2024-03-03 23:13:06 -0300

    Update page layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Title } from "@/components/Title";
 export default function Home() {
   return (
     <main>
-      <div className="min-h-screen relative overflow-x-hidden flex justify-center items-center">
-        <div className="absolute w-screen min-w-[700px] max-w-[2200px] h-[100%] right-0 xl:left-0">
+      <div className="min-h-dvh relative overflow-x-hidden flex justify-center items-center">
+        <div className="absolute w-dvw min-w-[700px] max-w-[2200px] h-[100%] right-0 xl:left-0">
           <Background />
         </div>
         <div className="w-[80%] xl:w-[1210px]mb-24">
